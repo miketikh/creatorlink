3a127ea9f037a74ee750d457ac5986942cfcf1cb
Mike Tikh <mike.tikh@gmail.com>
2025-10-20 14:35:11 -0500

    Initial Commit

---
+++ CreatorLink/CreatorLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatorLinkApp.swift
+//  CreatorLink
+//
+//  Created by gauntlet on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CreatorLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
